bb14c3a3f64be0b577e2d36436859f3686c1706f
Emiliano Garza <e.garza@blackstone.studio>
2020-11-06 17:55:46 -0600

    Fixed installation instructions

--- bin/blackstone-eslint-config.js
+++ bin/blackstone-eslint-config.js
@@ -7,6 +7,7 @@ const config = {
   ],
   rules: {},
 };
+const data = JSON.stringify(config, null, 4);
 
 const dependencyInstallation = spawn(
   'npm',
@@ -14,24 +15,29 @@ const dependencyInstallation = spawn(
     'install',
     '--save-dev',
     'eslint-config-airbnb',
-    'eslint@^#.#.#',
-    'eslint-plugin-jsx-a11y@^#.#.#',
-    'eslint-plugin-import@^#.#.#',
-    'eslint-plugin-react@^#.#.#',
-    'eslint-plugin-react-hooks@^#.#.#',
+    'eslint@latest',
+    'eslint-plugin-jsx-a11y@latest',
+    'eslint-plugin-import@latest',
+    'eslint-plugin-react@latest',
+    'eslint-plugin-react-hooks@latest',
     'eslint-plugin-jsdoc',
+    'eslint-config-blackstone',
   ],
 );
 
-const data = JSON.stringify(config, null, 4);
-dependencyInstallation.on('exit', () => {
-  fs.writeFile('.eslintrc', data, (err) => {
-    if (err) {
-      process.stdout.write('There was an error trying to create the eslint config file');
-    } else {
-      process.stdout.write('You\'re good to go macho man');
-    }
-  });
-});
-
 dependencyInstallation.stdout.pipe(process.stdout);
+dependencyInstallation.stderr.pipe(process.stdout);
+
+dependencyInstallation.on('exit', (code) => {
+  if (code !== 1) {
+    fs.writeFile('.eslintrc', data, (err) => {
+      if (err) {
+        process.stdout.write('There was an error trying to create the eslint config file\n');
+      } else {
+        process.stdout.write('You\'re good to go macho man\n');
+      }
+    });
+  } else {
+    process.stdout.write('An error has ocurred with npm\n');
+  }
+});

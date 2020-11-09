d1dc6b75ac438f08adbd42bebe13a9a103e4a8be
Emiliano Garza <e.garza@blackstone.studio>
2020-11-09 12:23:48 -0600

    New workaround

--- src/main.js
+++ src/main.js
@@ -1,16 +1,6 @@
 module.exports = {
-  extends: [
-    'plugin:react/recommended',
-    'plugin:jsdoc/recommended',
-    'airbnb',
-  ],
-  plugins: [
-    'react',
-    'react-hooks',
-    'jsdoc',
-  ],
   rules: {
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
     'react/jsx-props-no-spreading': 1,
   },
 };

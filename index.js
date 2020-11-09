f552fe025864605a7ab2d68d4595eafec9f41069
Emiliano Garza <e.garza@blackstone.studio>
2020-11-09 10:17:36 -0600

    Updaqted dependency config file

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-module.exports = {
-  extends: [
-    'plugin:react/recommended',
-    'plugin:jsdoc/recommended',
-    'airbnb',
-  ],
-  plugins: [
-    'react',
-    'jsdoc',
-  ],
-  rules: {
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-    'react/jsx-props-no-spreading': 1,
-  },
-};
+const main = require('./src/main');
+
+module.exports = main;
